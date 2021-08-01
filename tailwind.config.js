82ed2a1e97aca5df778461baf60639c4ade8013c
ryeenii <gustarrini@pm.me>
2021-08-01 15:27:17 -0300

    update tailwind properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,23 @@ module.exports = {
   },
   theme: {
     colors: {
-      yellow: "#ffe500",
+      yellow: { 
+        DEFAULT: "#FFE500",
+        light: "#271C07",
+        lightclr: "#FFEA33"
+      },
       orange: "#f28b27",
-      red: "#c429a9",
-      blue: "#4445ff",
+      blue: { 
+        DEFAULT: "#4445ff",
+        light: "#181428",
+        lightclr: "#696AFF"
+      },
       green: "#0bf131",
       purple: {
         dark: "#150411",
-        DEFAULT: "#150411",
+        DEFAULT: "#c429a9",
         light: "#281428",
+        lightclr: "#D054BA"
       },
       tp: {
         heavy: "rgba(255,255,255,0.05)",
@@ -42,6 +50,12 @@ module.exports = {
       transitionTimingFunction: {
         back: "cubic-bezier(0.68, -0.6, 0.32, 1.6)",
       },
+      scale: {
+        60: '0.6'
+      },
+      fontSize: {
+        '4.5xl': '2.5em',
+      }
     },
   },
   corePlugins: {
