74e0f20169013706402e7cc583fc76e6e0d2577b
ryeenii <gustarrini@pm.me>
2021-08-01 15:14:15 -0300

    include light colors function

--- src/libs/colors.ts
+++ src/libs/colors.ts
@@ -2,9 +2,11 @@ const gradients = [
   "linear-gradient(90deg, rgba(255,229,0,1) 0%, rgba(249,190,17,1) 100%)",
   "linear-gradient(90deg, rgba(242,139,39,1) 0%, rgba(249,190,17,1) 100%)",
   "linear-gradient(90deg, rgba(196,41,169,1) 0%, rgba(141,53,206,1) 100%)",
-  "linear-gradient(90deg,  rgba(68,69,255,1) 0%, rgba(51,121,193,1) 100%);",
-  "linear-gradient(90deg, rgba(11,241,49,1) 0%, rgba(32,179,123,1) 100%);",
+  "linear-gradient(90deg,  rgba(68,69,255,1) 0%, rgba(51,121,193,1) 100%)",
+  "linear-gradient(90deg, rgba(11,241,49,1) 0%, rgba(32,179,123,1) 100%)",
 ]
+const lightColors = [ "#FFEA33", "#F39132", "#D054BA", "#696AFF", "#3CF45A" ]
+const stdColors = [ "#FFE500", "#F28B27", "#C429A9", "#4445FF", "#0BF131" ]
 export class colors {
   async setGradient(grad: string) {
     return new Promise((resolve) => {
@@ -24,6 +26,48 @@ export class colors {
         case "green":
           resolve(gradients[4])
           break
+      }
+    })
+  }
+  async setLight(clr: string) {
+    return new Promise((resolve) => {
+      switch(clr) {
+        case "yellow":
+          resolve(lightColors[0])
+          break;
+        case "orange":
+          resolve(lightColors[1])
+          break;
+        case "purple":
+          resolve(lightColors[2])
+          break;
+        case "blue":
+          resolve(lightColors[3])
+          break;
+        case "yellow":
+          resolve(lightColors[4])
+          break;
+      }
+    });
+  }
+  async splitHex(clr: string) {
+    return new Promise((resolve) => {
+      switch(clr) {
+        case "yellow":
+          resolve([stdColors[0], stdColors[1]])
+          break;
+        case "orange":
+          resolve([stdColors[1], stdColors[0]])
+          break;
+        case "purple":
+          resolve([stdColors[2], stdColors[3]])
+          break;
+        case "blue":
+          resolve(["#3379C1", stdColors[3]])
+          break;
+        case "green":
+          resolve([stdColors[4], stdColors[0]])
+          break;
       }
     })
   }
