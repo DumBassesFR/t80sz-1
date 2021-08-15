ad626e7a66aab2402a18ab23ab8eb017b6b18ba6
ryeenii <gustarrini@pm.me>
2021-08-15 18:38:51 -0300

    prettier things
    i forgo :skull: to add the prettier plugin to my vim installation, thankfully it's all good now lole

--- src/svelte/pages/info/info.svelte
+++ src/svelte/pages/info/info.svelte
@@ -3,15 +3,15 @@
   import "../../../css/pages.pcss"
   import AboutUs from "./docs/AboutUs.svx"
   import AboutTheTeam from "./docs/AboutTeam.svx"
-  import BackHome from "../general/templates/back-home.svelte";
-  import Aura from "../general/templates/aura.svelte";
-  import { fly, fade } from 'svelte/transition'
+  import BackHome from "../general/templates/back-home.svelte"
+  import Aura from "../general/templates/aura.svelte"
+  import { fly, fade } from "svelte/transition"
   const array = [AboutUs, AboutTheTeam]
-  let selItem: object = AboutUs;
+  let selItem: object = AboutUs
   const clrs = ["#ffe500", "#f29b27", "#c429a9", "#4445ff", "#0bf131"]
   let slrClr = ""
   let slrVal = 5
-  let slidr;
+  let slidr
   class Slider {
     async splitArr() {
       return new Promise((resolve) => {
@@ -73,7 +73,7 @@
         const vTS = (obj: object) => Object.keys(obj)[0]
         promSplit[0].forEach((element: object) => {
           iad[it] = {}
-          iad[it].text = element.name 
+          iad[it].text = element.name
           iad[it].el = element
           iad[it].no = promSplit[0].length
           iad[it].crno = it
@@ -101,134 +101,136 @@
       return slrClr
     }
     changeVal(val: object) {
-      selItem = val;
+      selItem = val
     }
-    changePos(val : object[], no : number) {
-      switch(no) {
+    changePos(val: object[], no: number) {
+      switch (no) {
         case 4:
-          slidr.value = 91;
-          break;
+          slidr.value = 91
+          break
         case 3:
           if (val.length === 5) {
-            slidr.value = 76;
+            slidr.value = 76
+          } else {
+            slidr.value = 91
           }
-          else {
-            slidr.value = 91;
-          }
-          break;
+          break
         case 2:
           if (val.length >= 4) {
-            slidr.value = 36;
+            slidr.value = 36
+          } else {
+            slidr.value = 91
           }
-          else {
-            slidr.value = 91;
-          }
-          break;
+          break
         case 1:
           if (val.length > 2) {
-            slidr.value = 16;
+            slidr.value = 16
+          } else {
+            slidr.value = 91
           }
-          else {
-            slidr.value = 91;
-          }
-          break;
+          break
         case 0:
-          slidr.value = 5;
+          slidr.value = 5
       }
-      this.colorSwap(slidr.value);
+      this.colorSwap(slidr.value)
     }
     async hitBox(val: number) {
-     return new Promise(resolve => { 
-      if (val >= 0 && val <= 15) {
-        resolve(0);
-      } else if (val >= 15 && val <= 35) {
-        const ftc = this.splitArr();
-        ftc.then((value : object[]) => {
-          if (value.length == 5 || value.length == 4) {
-            resolve(1);
-          }
-          else {
-            resolve(0);
-          }
-        });
-      } else if (val >= 35 && val <= 75) {
-        const ftc = this.splitArr();
-        ftc.then((value : object[]) => {
-          if (value.length >= 3) {
-            resolve(2);
-          }
-          else {
-            resolve(0);
-          }
-        })
-      } else if (val >= 75 && val <= 95) {
-        const ftc = this.splitArr();
-        ftc.then((value : object[]) => {
-          if (value.length >= 4) {
-            resolve(3);
-          }
-          else {
-            resolve(1);
-          }
-        })
-      } else if (val >= 95) {
-        const ftc = this.splitArr();
-        ftc.then((value : object[]) => {
-          resolve(value.length - 1);
-        })
-      }
-     }) 
+      return new Promise((resolve) => {
+        if (val >= 0 && val <= 15) {
+          resolve(0)
+        } else if (val >= 15 && val <= 35) {
+          const ftc = this.splitArr()
+          ftc.then((value: object[]) => {
+            if (value.length == 5 || value.length == 4) {
+              resolve(1)
+            } else {
+              resolve(0)
+            }
+          })
+        } else if (val >= 35 && val <= 75) {
+          const ftc = this.splitArr()
+          ftc.then((value: object[]) => {
+            if (value.length >= 3) {
+              resolve(2)
+            } else {
+              resolve(0)
+            }
+          })
+        } else if (val >= 75 && val <= 95) {
+          const ftc = this.splitArr()
+          ftc.then((value: object[]) => {
+            if (value.length >= 4) {
+              resolve(3)
+            } else {
+              resolve(1)
+            }
+          })
+        } else if (val >= 95) {
+          const ftc = this.splitArr()
+          ftc.then((value: object[]) => {
+            resolve(value.length - 1)
+          })
+        }
+      })
     }
   }
   const slr = new Slider()
   const fetchAll = slr.combineAll()
 </script>
 
-<div class="page-gap" in:fly={{y: 1200, duration:1000}} out:fade={{duration: 500}}>
-<div class="variant-button" style="display: flex;">
-  <BackHome gradient="yellow"/>
-  <div class="chap-button" style="background-image: linear-gradient(90deg, rgba(255,229,0,1) 0%, rgba(249,190,17,1) 100%);">
-    <p class="chap-icon">book</p>
-    <p class="chap-text">chapters</p>
-  </div>
-</div>
-<div class="docs-gap" name="info">
-  <svelte:component this={selItem} />
-</div>
-<Aura color="yellow"/>
-{#await fetchAll}
-  <p>loading elements</p>
-{:then arr}
-  <div class="chapters">
-    <div class="slider">
-      <input
-        type="range"
-        bind:value={slrVal}
-        bind:this={slidr}
-        on:mouseup={() => {
-          const hb = slr.hitBox(slrVal);  
-          hb.then(hbx => {
-            slr.changeVal(arr[hbx].el)
-          })
-        }} 
-        on:input={() => slr.colorSwap(slrVal)}
-        class="rainbow-slider"
-        style="--slr-clr: {slrClr || clrs[0]};" />
-    </div>
-    <div class="offset-{arr[0].no} chapter-contents">
-      {#each arr as el}
-        <p
-          style="--selected-color: {el.color}"
-          on:click={() => { slr.changeVal(el.el); slr.changePos(arr, el.crno);}}>
-          {el.crno + 1}. {el.text
-            .match(/[A-Z][a-z]+|[0-9]+/g)
-            .join(" ")
-            .toLowerCase()}
-        </p>
-      {/each}
+<div
+  class="page-gap"
+  in:fly={{ y: 1200, duration: 1000 }}
+  out:fade={{ duration: 500 }}>
+  <div class="variant-button" style="display: flex;">
+    <BackHome gradient="yellow" />
+    <div
+      class="chap-button"
+      style="background-image: linear-gradient(90deg, rgba(255,229,0,1) 0%, rgba(249,190,17,1) 100%);">
+      <p class="chap-icon">book</p>
+      <p class="chap-text">chapters</p>
     </div>
   </div>
-{:catch}
-  <p>pp, something went wrong</p>
-{/await}
+  <div class="docs-gap" name="info">
+    <svelte:component this={selItem} />
+  </div>
+  <Aura color="yellow" />
+  {#await fetchAll}
+    <p>loading elements</p>
+  {:then arr}
+    <div class="chapters">
+      <div class="slider">
+        <input
+          type="range"
+          bind:value={slrVal}
+          bind:this={slidr}
+          on:mouseup={() => {
+            const hb = slr.hitBox(slrVal)
+            hb.then((hbx) => {
+              slr.changeVal(arr[hbx].el)
+            })
+          }}
+          on:input={() => slr.colorSwap(slrVal)}
+          class="rainbow-slider"
+          style="--slr-clr: {slrClr || clrs[0]};" />
+      </div>
+      <div class="offset-{arr[0].no} chapter-contents">
+        {#each arr as el}
+          <p
+            style="--selected-color: {el.color}"
+            on:click={() => {
+              slr.changeVal(el.el)
+              slr.changePos(arr, el.crno)
+            }}>
+            {el.crno + 1}. {el.text
+              .match(/[A-Z][a-z]+|[0-9]+/g)
+              .join(" ")
+              .toLowerCase()}
+          </p>
+        {/each}
+      </div>
+    </div>
+  {:catch}
+    <p>pp, something went wrong</p>
+  {/await}
 </div>
--- src/svelte/pages/varchive/varchive.svelte
+++ src/svelte/pages/varchive/varchive.svelte
@@ -1,37 +1,45 @@
-<script>
-  import Header from '../general/templates/header.svelte';
-  import BackHome from '../general/templates/back-home.svelte';
-  import LargeRedirect from './templates/lg-redirect.svelte';
-  import Docs from './docs/varchive.svx'
-  import Aura from '../general/templates/aura.svelte';
-  import { fade, fly } from 'svelte/transition'
-  import '../../../css/pages.pcss'
-</script>
 <style>
-	.redirect-divider {
-    @apply flex flex-col sm:flex-row; 
-		width: -webkit-fit-content;
-		width: fit-content;
-		width: -moz-fit-content;
-	}
+  .redirect-divider {
+    @apply flex flex-col sm:flex-row;
+    width: -webkit-fit-content;
+    width: fit-content;
+    width: -moz-fit-content;
+  }
 </style>
-<div class="page-gap" in:fly={{y: 500, duration: 1000}} out:fade={{duration: 200}}>
-  <BackHome gradient="blue"/>
-  <Header gradient="blue" text="video archive"/>
-  <div class=docs-gap>
-    <Docs/>
+
+<script>
+  import Header from "../general/templates/header.svelte"
+  import BackHome from "../general/templates/back-home.svelte"
+  import LargeRedirect from "../templates/lg-redirect.svelte"
+  import Docs from "./docs/varchive.svx"
+  import Aura from "../general/templates/aura.svelte"
+  import { fade, fly } from "svelte/transition"
+  import "../../../css/pages.pcss"
+</script>
+
+<div
+  class="page-gap"
+  in:fly={{ y: 500, duration: 1000 }}
+  out:fade={{ duration: 200 }}>
+  <BackHome gradient="blue" />
+  <Header gradient="blue" text="video archive" />
+  <div class="docs-gap">
+    <Docs />
   </div>
-  <div class="redirect-divider" in:fly={{y: 500, duration: 1000}} out:fade={{duration: 200}}>
+  <div
+    class="redirect-divider"
+    in:fly={{ y: 500, duration: 1000 }}
+    out:fade={{ duration: 200 }}>
     <LargeRedirect
       icon="account_balance"
-     text="The Internet Archive Mirror" 
-     color="yellow"
-     red="https://archive.org/details/@the_80s_zone"/>
+      text="The Internet Archive Mirror"
+      color="yellow"
+      red="https://archive.org/details/@the_80s_zone" />
     <LargeRedirect
-     icon="play_circle_filled"
-     text="YouTube Mirror"
-     color="purple"
-     red="https://www.youtube.com/channel/UC_aA045hM9gjf4PamMJtAkA"/>
+      icon="play_circle_filled"
+      text="YouTube Mirror"
+      color="purple"
+      red="https://www.youtube.com/channel/UC_aA045hM9gjf4PamMJtAkA" />
   </div>
 </div>
-<Aura color="blue"/>
+<Aura color="blue" />
