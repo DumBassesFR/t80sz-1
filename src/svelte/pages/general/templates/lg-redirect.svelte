4b83f6daadec64f62bffdd5f7066d69926b5c721
ryeenii <gustarrini@pm.me>
2021-09-29 16:49:30 -0300

    per-color backgrounds for LargeRedirect

--- src/svelte/pages/general/templates/lg-redirect.svelte
+++ src/svelte/pages/general/templates/lg-redirect.svelte
@@ -5,19 +5,31 @@
   const clrOutput = clr.setLight(`${color}`);
 </script>
 <style lang="postcss">
+  .green {
+    @apply bg-green-light;
+  }
+  .blue {
+    @apply bg-blue-light;
+  }
+  .yellow {
+    @apply bg-yellow-light;
+  }
+  .purple {
+    @apply bg-purple-light;
+  }
   .large-redirect {
-    @apply text-sm my-2 sm:mr-2 sm:my-5 2xl:my-8 px-2 2xl:text-3xl 2xl:p-5 2xl:px-6 bg-blue-light h-10 flex flex-row rounded shadow;
+    @apply text-sm my-2 sm:mr-2 sm:my-5 2xl:my-8 px-2 2xl:text-xl 2xl:p-2 2xl:px-4 h-10 flex flex-row rounded shadow;
     .redirect {
       @apply font-sans leading-3;
     }
     .redirect-icon {
-      @apply font-icon text-lg 2xl:text-4xl mr-1; 
+      @apply font-icon text-lg 2xl:text-3xl mr-1; 
       line-height: 0;
     }
   }
 </style>
 <a href="{red}" style="color: inherit;">
-<div class="large-redirect">
+<div class="large-redirect {color}">
   {#await clrOutput}
     <p>loading</p>
   {:then col}
