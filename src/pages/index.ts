d435bf4dcbf159384ab617656bf846905f9ce1e9
ryeenii <gustarrini@pm.me>
2021-10-04 21:23:11 -0300

    update console.log

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,7 @@
 import "svelte"
 import Base from "../svelte/base.svelte"
+import { colors } from "../libs/colors";
+const col = new colors();
 async function loadElements() {
   new Base({
     target: document.body,
@@ -7,11 +9,22 @@ async function loadElements() {
 }
 loadElements().then(() => {
   console.log(
-    "%cThe 80s Zone",
-    "color: fuchsia; font-size: 3em; font-family: Comic Sans MS;"
-  )
-  console.log(
-    "%cbest site in the game scene",
-    "color: fuchsia; font-family: Comic Sans MS;"
-  )
+    `%c
+          |
+     ▁▁▁▁▁▁▁▁▁▁
+    /          \\
+   | ▁  . ◡ . ▁ | The 80s Zone, ${new Date(Date.now()).getFullYear()}
+   ██████████████ Contribute to this site! Read the Source page 
+    \\__________/  for more information!
+    `,
+    `font-weight: bold; 
+     font-size: 10px;
+     color: white; 
+     text-shadow: 1.5px 1.5px 0 black, 
+     2px 2px 0 ${col.lightColors[0]},
+     4px 4px 0 ${col.lightColors[1]}, 
+     6px 6px 0 ${col.lightColors[2]}, 
+     7px 7px 0 ${col.lightColors[3]}; 
+     margin-bottom: 12px;`
+  );
 })
