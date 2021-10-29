838cd852c0dfd4159c533cb7455fd6e4cf36ae78
ryeenii <gustarrini@pm.me>
2021-10-29 15:43:48 -0300

    implement nojs code template

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const cssExtract = require("mini-css-extract-plugin")
 const cssMinimize = require("css-minimizer-webpack-plugin")
 const htmlWebpack = require("html-webpack-plugin")
-const copyPlug = require("copy-webpack-plugin");
+const copyPlug = require("copy-webpack-plugin")
 const compress = require("compression-webpack-plugin")
 const path = require("path")
 const defcfg = require("./webpack.config")
@@ -11,7 +11,7 @@ module.exports = merge(defcfg, {
   entry: {
     main: "./src/pages/index.ts",
     bobby: "./src/pages/standalone-bobby.ts",
-    404: "./src/pages/status-codes/404.ts"
+    404: "./src/pages/status-codes/404.ts",
   },
   output: {
     filename: "src/t80sz.[name].js",
@@ -26,6 +26,26 @@ module.exports = merge(defcfg, {
         description: "EST. 2017-10-31",
         "theme-color": "#150411",
       },
+      templateContent: ({ htmlWebpackPlugin }) => `
+      <html>
+        <head>
+        </head>
+        <body>
+          <noscript>
+            <div style="color: #ffffff; width:100%; height: 100%; display: flex; justify-content: center; align-items: center; text-align: center;">
+            <div style="width: 50%;">
+              <h1 style="color: #ffe500;">It looks like you don't have Javascript enabled!</h1>
+              <p>As much as we respect your opinion (especially with tons of websites invading privacy using such language),</p>
+              <p>our default site does not functions without JS toggled.</p>
+              <br>
+              <p>We promise that we do not include any sorts of shady tracking services, or even store your data in general as we genuinely despise privacy invasion. (our source code is open to allow you to research this!)</p>
+              <p>If you still prefer having javascript disabled, though.. <a href="/nojs">We have something special for you.</a></p>
+              </div>
+            </div>
+          </noscript>
+        </body>
+      </html>
+      `,
       excludeChunks: ["bobby", "404"],
       hash: true,
     }),
@@ -57,10 +77,10 @@ module.exports = merge(defcfg, {
     new compress({ exclude: /node_modules/, compressionOptions: { level: 7 } }),
     new copyPlug({
       patterns: [
-        { from: 'src/assets/team/', to: 'src/assets/team' },
-        { from: 'src/nojs', to: 'nojs/' }
-      ]
-    })
+        { from: "src/assets/team/", to: "src/assets/team" },
+        { from: "src/nojs", to: "nojs/" },
+      ],
+    }),
   ],
   module: {
     rules: [
@@ -71,19 +91,19 @@ module.exports = merge(defcfg, {
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
           filename: "src/fonts/[hash][ext][query]",
-          publicPath: "../"
-        }
+          publicPath: "../",
+        },
       },
       {
         test: /\.(webp|png|jpg|mp3)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
           filename: "src/assets/[name][ext][query]",
-          publicPath: "../"
-        }
+          publicPath: "../",
+        },
       },
       {
         test: /\.(html)$/i,
